eb0b7a131714ef2af30128fa260c943fdef3d4d6
volodic <volodiaint@gmai.com>
2025-05-14 19:03:06 +0300

    Update core

--- test_script/stereo2/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .raft_stereo_wrapper import RaftStereoWrapper
-from .utils import download_model_weights, load_model, get_device_info
-
-__all__ = [
-    'RaftStereoWrapper',
-    'download_model_weights',
-    'load_model',
-    'get_device_info'
-]
