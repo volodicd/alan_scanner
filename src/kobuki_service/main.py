8a4be3bb1d135d0ef49bcfd06d5c1d1ccaa9390e
volodic <volodiaint@gmai.com>
2025-05-14 19:35:51 +0300

    adding also kobukit part

--- src/kobuki_service/main.py
+++ src/kobuki_service/main.py
@@ -8,7 +8,10 @@ app = flask.Flask(__name__)
 x,y = app.route('/move/<int:x>/<int:y>')
 
 turtle_bot = TurtleBotAStar()
-
+# is_object -> bool -> object on the way
+# distance_object -> int -> distance to center obj
+# objs -> list -> avarage distance of 16 squares on pictures
+# final -> ? bool
 turtle_bot.move(x,y)
 
 
